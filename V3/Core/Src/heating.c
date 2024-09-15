592125e778e11d736b10c6d93afaa0bc24fa3890
PC_Fish <fishorz2014@gmail.com>
2024-09-15 22:36:50 +0800

    add heater PID

---
+++ V3/Core/Src/heating.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "heating.h"
+
+void init_pid(HEATING_TypeDef *heating){
+
+	heating->Kp = 0.1;
+	heating->Ki = 0;
+	heating->Kd = 0;
+	heating->dt = 0.1f;
+}
+
+void cal_pid(HEATING_TypeDef *heating, uint32_t nowTemp, uint32_t targetTemp){
+
+	heating->error  =  targetTemp - nowTemp;
+	heating->integralError += heating->error + heating->previousError;
+	heating->derivative = (heating->error - heating->previousError)/heating->dt;
+	heating->uPid = heating->P + heating->I + heating->D;
+
+	if(heating->uPid > 1.0)
+		{
+		heating->uPid = 1.0;
+		}
+		if(heating->uPid < 0.0)
+		{
+			heating->uPid = 0.0;
+		}
+		float duty = heating->uPid * 1000.0;
+		heating->pwm_duty = (int) duty;
+		heating->previousError = heating->error;
+}
