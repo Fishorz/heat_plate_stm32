799d00157d09b901f38dfe33b3e09681b77cc670
PC_Fish <fishorz2014@gmail.com>
2024-10-03 12:50:09 +0800

    add int temp to config

--- V3/Core/Src/main.c
+++ V3/Core/Src/main.c
@@ -29,6 +29,7 @@
 #include "meun.h"
 #include"NTCtempSensor.h"
 #include "heating.h"
+#include "config.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -117,7 +118,7 @@ int main(void)
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
 	HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_ALL);
 	HAL_ADC_Start_DMA(&hadc1, &adcValue, 1);
-	meunInit(&meun, 150);
+	meunInit(&meun, INIT_TEMP);
 	init_pid(&heating);
   /* USER CODE END 2 */
 
